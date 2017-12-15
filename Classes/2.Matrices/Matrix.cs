c55b0a0428eee958b4b4ab47f0c2103d35f05328
Alexander Markov <alexander_29@abv.bg>
2017-12-15 17:01:19 +0200

    Matrix change

--- Classes/2.Matrices/Matrix.cs
+++ Classes/2.Matrices/Matrix.cs
@@ -15,14 +15,10 @@ namespace _2.Matrices
         public Matrix(int rows, int cols)
         {
             if (rows < 0)
-            {
                 throw new ArgumentException("Rows shoud be positive number.");
-            }
 
             if (cols < 0)
-            {
                 throw new ArgumentException("Cols shoud be positive number.");
-            }
 
             this.rows = rows;
             this.cols = cols;
@@ -32,32 +28,22 @@ namespace _2.Matrices
 
         public Matrix(T[,] matrix)
         {
-            int rows = matrix.GetLength(0);
-            int cols = matrix.GetLength(1);
+            var rows = matrix.GetLength(0);
+            var cols = matrix.GetLength(1);
 
             this.matrix = new T[rows, cols];
 
-            for (int row = 0; row < rows; row++)
-            {
-                for (int col = 0; col < cols; col++)
-                {
-                    this.matrix[row, col] = matrix[row, col];
-                }
-            }
+            for (var row = 0; row < rows; row++)
+            for (var col = 0; col < cols; col++)
+                this.matrix[row, col] = matrix[row, col];
 
             this.rows = rows;
             this.cols = cols;
         }
 
-        public int Rows
-        {
-            get { return this.rows; }
-        }
+        public int Rows => this.rows;
 
-        public int Cols
-        {
-            get { return cols; }
-        }
+        public int Cols => this.cols;
 
         public T this[int row, int col]
         {
@@ -76,22 +62,16 @@ namespace _2.Matrices
         public static Matrix<T> operator +(Matrix<T> m1, Matrix<T> m2)
         {
             if (m1.Rows != m2.Rows || m1.Cols != m2.Cols)
-            {
                 throw new ArithmeticException("Matrix dimensions shoud be the same.");
-            }
 
-            int rows = m1.Rows;
-            int cols = m1.Cols;
+            var rows = m1.Rows;
+            var cols = m1.Cols;
 
             var result = new Matrix<T>(rows, cols);
 
-            for (int row = 0; row < rows; row++)
-            {
-                for (int col = 0; col < cols; col++)
-                {
-                    result[row, col] = (dynamic)m1[row, col] + (dynamic)m2[row, col];
-                }
-            }
+            for (var row = 0; row < rows; row++)
+            for (var col = 0; col < cols; col++)
+                result[row, col] = (dynamic)m1[row, col] + (dynamic)m2[row, col];
 
             return result;
         }
@@ -99,70 +79,50 @@ namespace _2.Matrices
         public static Matrix<T> operator -(Matrix<T> m1, Matrix<T> m2)
         {
             if (m1.Rows != m2.Rows || m1.Cols != m2.Cols)
-            {
                 throw new ArithmeticException("Matrix dimensions shoud be the same.");
-            }
 
-            int rows = m1.Rows;
-            int cols = m1.Cols;
+            var rows = m1.Rows;
+            var cols = m1.Cols;
 
             var result = new Matrix<T>(rows, cols);
 
-            for (int row = 0; row < rows; row++)
-            {
-                for (int col = 0; col < cols; col++)
-                {
-                    result[row, col] = (dynamic)m1[row, col] - (dynamic)m2[row, col];
-                }
-            }
+            for (var row = 0; row < rows; row++)
+            for (var col = 0; col < cols; col++)
+                result[row, col] = (dynamic)m1[row, col] - (dynamic)m2[row, col];
 
             return result;
         }
 
         public static Matrix<T> operator *(Matrix<T> firstMatrix, Matrix<T> secondMatrix)
         {
-            int firstMatrixRows = firstMatrix.Rows;
-            int firstMatrixCols = firstMatrix.Cols;
+            var firstMatrixRows = firstMatrix.Rows;
+            var firstMatrixCols = firstMatrix.Cols;
 
-            int secondMatrixRows = secondMatrix.Rows;
-            int secondMatrixCols = secondMatrix.Cols;
+            var secondMatrixRows = secondMatrix.Rows;
+            var secondMatrixCols = secondMatrix.Cols;
 
             if (firstMatrixCols != secondMatrixRows)
-            {
                 throw new ArithmeticException("Matrices cannot be multiplied.");
-            }
 
             var resultMatrix = new Matrix<T>(firstMatrixRows, secondMatrixCols);
 
-            for (int i = 0; i < firstMatrixRows; i++)
-            {
-                for (int j = 0; j < secondMatrixCols; j++)
-                {
-                    for (int k = 0; k < firstMatrixCols; k++)
-                    {
-                        resultMatrix[i, j] += (dynamic)firstMatrix[i, k] * (dynamic)secondMatrix[k, j];
-                    }
-                }
-            }
+            for (var i = 0; i < firstMatrixRows; i++)
+            for (var j = 0; j < secondMatrixCols; j++)
+            for (var k = 0; k < firstMatrixCols; k++)
+                resultMatrix[i, j] += (dynamic)firstMatrix[i, k] * (dynamic)secondMatrix[k, j];
 
             return resultMatrix;
         }
 
         public static bool operator true(Matrix<T> matrix)
         {
-            int rows = matrix.Rows;
-            int cols = matrix.Cols;
+            var rows = matrix.Rows;
+            var cols = matrix.Cols;
 
-            for (int row = 0; row < rows; row++)
-            {
-                for (int col = 0; col < cols; col++)
-                {
-                    if ((dynamic)matrix[row, col] != 0)
-                    {
-                        return true;
-                    }
-                }
-            }
+            for (var row = 0; row < rows; row++)
+            for (var col = 0; col < cols; col++)
+                if ((dynamic)matrix[row, col] != 0)
+                    return true;
 
             return false;
         }
@@ -170,33 +130,25 @@ namespace _2.Matrices
         public static bool operator false(Matrix<T> matrix)
         {
 
-            int rows = matrix.Rows;
-            int cols = matrix.Cols;
+            var rows = matrix.Rows;
+            var cols = matrix.Cols;
 
-            for (int row = 0; row < rows; row++)
-            {
-                for (int col = 0; col < cols; col++)
-                {
-                    if ((dynamic)matrix[row, col] != 0)
-                    {
-                        return false;
-                    }
-                }
-            }
+            for (var row = 0; row < rows; row++)
+            for (var col = 0; col < cols; col++)
+                if ((dynamic)matrix[row, col] != 0)
+                    return false;
 
             return true;
         }
 
         public override string ToString()
         {
-            StringBuilder result = new StringBuilder();
+            var result = new StringBuilder();
 
-            for (int row = 0; row < this.Rows; row++)
+            for (var row = 0; row < this.Rows; row++)
             {
-                for (int col = 0; col < this.Cols; col++)
-                {
+                for (var col = 0; col < this.Cols; col++)
                     result.Append(this.matrix[row, col] + " ");
-                }
                 result.Append(Environment.NewLine);
             }
 
@@ -206,13 +158,9 @@ namespace _2.Matrices
         private void ValidateIndexes(int row, int col)
         {
             if (row < 0 || row >= this.Rows)
-            {
                 throw new IndexOutOfRangeException("Given row index is out of range.");
-            }
             if (col < 0 || col >= this.Cols)
-            {
                 throw new IndexOutOfRangeException("Given col index is out of range.");
-            }
         }
     }
 }
